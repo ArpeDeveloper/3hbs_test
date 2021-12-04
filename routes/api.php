627aca35a63727c0875cfe666957e17b28365f0d
Jesus Perez <jesus.ross@outlook.com>
2021-12-04 13:33:50 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AirportController;
+use App\Http\Controllers\AirlineController;
+use App\Http\Controllers\FlightController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +18,12 @@ use App\Http\Controllers\AuthController;
 |
 */
 Route::post('/login', [AuthController::class, 'login'])->name("api.login");
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name("api.logout");
+    Route::resource('/airports', AirportController::class)->except(["create","edit"]);
+    Route::resource('/airlines', AirlineController::class)->except(["create","edit"]);
+    Route::resource('/flights', FlightController::class)->except(["create","edit"]);
+
+});
